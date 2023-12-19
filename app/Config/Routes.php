bf6cdb865f55cd72ad771ae59149bc9075a73555
ibndana02 <muhammadibnuwardana@gmail.com>
2023-12-19 10:07:14 +0700

    change api to aladhan

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -33,38 +33,12 @@ $routes->get('/', 'Auth::index');
 $routes->post('/login', 'Auth::auth');
 $routes->get('/dashboard', 'Dashboard::index', ['filter' => 'auth']);
 $routes->get('/logout', 'Auth::logout', ['filter' => 'auth']);
-$routes->get('/display', 'Dashboard::display');
+$routes->get('/display', 'Dashboard::displayNew');
+$routes->get('/test', 'Dashboard::test');
 $routes->group('aplikasi', function ($routes) {
     $routes->get('/', 'Aplikasi::index', ['filter' => 'auth']);
     $routes->post('update', 'Aplikasi::update');
 });
-$routes->group('nasabah', function ($routes) {
-    $routes->get('/', 'Nasabah::nasabah', ['filter' => 'auth']);
-    $routes->match(['post', 'get'], 'list', 'Nasabah::nasabahList', ['filter' => 'auth']);
-    $routes->match(['post', 'get'], 'create', 'Nasabah::createNasabah', ['filter' => 'auth']);
-    $routes->match(['post', 'get'], 'create_badan', 'Nasabah::createBadan', ['filter' => 'auth']);
-    $routes->match(['post', 'get'], 'find', 'Nasabah::cariNasabah', ['filter' => 'auth']);
-    $routes->match(['post', 'get'], 'detailCore', 'Nasabah::detailCore', ['filter' => 'auth']);
-    $routes->match(['post', 'get'], 'detailBadan', 'Nasabah::detailBadan', ['filter' => 'auth']);
-    $routes->match(['post', 'get'], 'detail', 'Nasabah::detail', ['filter' => 'auth']);
-    $routes->match(['post', 'get'], 'registered', 'Nasabah::registered', ['filter' => 'auth']);
-    $routes->match(['post', 'get'], 'save', 'Nasabah::save', ['filter' => 'auth']);
-    $routes->match(['post', 'get'], 'delete', 'Nasabah::hapus', ['filter' => 'auth']);
-});
-$routes->group('users', function ($routes) {
-    $routes->get('/', 'Users::index', ['filter' => 'auth']);
-    $routes->match(['post', 'get'], 'create', 'Users::create', ['filter' => ['auth', 'admin']]);
-    $routes->match(['post', 'get'], 'list', 'Users::list', ['filter' => 'auth']);
-    $routes->match(['post', 'get'], 'save', 'Users::save', ['filter' => 'auth']);
-    $routes->match(['post'], 'detail', 'Users::detail', ['filter' => 'auth']);
-    $routes->match(['post', 'get'], 'delete', 'Users::delete', ['filter' => 'auth']);
-    $routes->match(['post'], 'update', 'Users::update', ['filter' => 'auth']);
-});
-$routes->group('profil', function ($routes) {
-    $routes->get('/', 'Users::profil', ['filter' => 'auth']);
-    $routes->post('update', 'Users::profilUpdate', ['filter' => 'auth']);
-});
-
 
 
 /*
--- app/Controllers/Dashboard.php
+++ app/Controllers/Dashboard.php
@@ -38,6 +38,30 @@ class Dashboard extends BaseController
         echo view('display_lte', $this->data);
     }
 
+    public function displayNew()
+    {
+        $this->data['title'] = 'Display Information';
+        $baghas = $this->Basil_m->getBaghas(date('Y-m-d'));
+        $this->data['baghas'] = $baghas;
+        $api_jadwal = json_decode($this->apiJadwal());
+        $jadwal = $api_jadwal->data->timings;
+        if ($api_jadwal->status) {
+            $this->data['jadwalSholat'] = true;
+            $waktu = [
+                ['sholat' => 'subuh', 'waktu' => $jadwal->Fajr, 'warna' => 'bg-purple'],
+                ['sholat' => 'syuruq', 'waktu' => $jadwal->Sunrise, 'warna' => 'bg-teal'],
+                ['sholat' => 'dzuhur', 'waktu' => $jadwal->Dhuhr, 'warna' => 'bg-warning'],
+                ['sholat' => 'ashar', 'waktu' => $jadwal->Asr, 'warna' => 'bg-info'],
+                ['sholat' => 'maghrib', 'waktu' => $jadwal->Maghrib, 'warna' => 'bg-maroon'],
+                ['sholat' => 'isya', 'waktu' => $jadwal->Isha, 'warna' => 'bg-gray-dark'],
+            ];
+            $this->data['waktu'] = $waktu;
+        } else {
+            $this->data['jadwalSholat'] = false;
+        }
+        echo view('display_lte', $this->data);
+    }
+
     public function getJadwalSholat($date)
     {
         $tahun = date('Y', strtotime($date));
@@ -63,5 +87,41 @@ class Dashboard extends BaseController
 
         curl_close($curl);
         return $response;
+    }
+
+    public function apiJadwal()
+    {
+        $api = $this->data['app']->api_jadwal_sholat;
+
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => "$api",
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'GET',
+        ));
+
+        $response = curl_exec($curl);
+
+        curl_close($curl);
+        return $response;
+    }
+
+    public function test()
+    {
+        $file = base_url('adhan.json');
+        $adhan = file_get_contents($file);
+        $aladhan = json_decode($adhan);
+        $jadwal = $aladhan->data->timings;
+        echo "Shubuh : " . $jadwal->Fajr . " WIB</br>";
+        echo "Dzuhur : " . $jadwal->Dhuhr . " WIB</br>";
+        echo "Ashar : " . $jadwal->Asr . " WIB</br>";
+        echo "Maghrib : " . $jadwal->Maghrib . " WIB</br>";
+        echo "Isya : " . $jadwal->Isha . " WIB</br>";
     }
 }
